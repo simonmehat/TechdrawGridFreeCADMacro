d46f79c2edec4704219d04d18186cda4ddd25b4b
simonmehat <simon.mehat@gmail.com>
2023-07-19 11:39:10 +0200

    Update add comment

--- Create_Origin.py
+++ Create_Origin.py
@@ -1,21 +1,24 @@
-#Create_origin
+# Create_origin
 
-def CreateOriginBox():
-    box = App.ActiveDocument.addObject("Part::Box", "origin")
-    box.Length = 10
+def CreateOriginBox():  # Create the orgin box
+    box = App.ActiveDocument.addObject("Part::Box", "origin")  # create a box
+    box.Length = 10  # set the dimension
     box.Width = 10
     box.Height = 10
-    box.Placement = FreeCAD.Placement(FreeCAD.Vector(-100, -100, 0), FreeCAD.Rotation(0, 0, 0))
+    box.Placement = FreeCAD.Placement(
+        FreeCAD.Vector(-100, -100, 0), FreeCAD.Rotation(0, 0, 0))  # place the box
     box.recompute()
     box = App.ActiveDocument.getObjectsByLabel("origin")[0]
+    # add length of the grid property
     box.addProperty("App::PropertyDistance", "X_grid_size")
     box.addProperty("App::PropertyDistance", "Y_grid_size")
-    box.X_grid_size = 2000
+    box.X_grid_size = 2000  # set default value
     box.Y_grid_size = 2000
 
-def CreateSpreadsheet():
+
+def CreateSpreadsheet():  # Create a spreadsheet
     sheet = App.ActiveDocument.addObject("Spreadsheet::Sheet", "Grid")
-    sheet.set('B1', '0')
+    sheet.set('B1', '0')  # add default value
     sheet.set('D1', '0')
     sheet.set('B2', '100')
     sheet.set('B3', '200')
